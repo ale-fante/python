fdfe32a10f2409c346c66f3c3e62fe6bb6b6e7be
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-29 14:58:45 -0700

    Write to text file

--- write_to_file.py
+++ write_to_file.py
@@ -1,4 +1,4 @@
 filename = "programming.txt"
 
 with open(filename, 'w') as file_object:
-  file_object.write("One of our simplest ways to save data is to write it to a file")
+  file_object.write("Test test test")
