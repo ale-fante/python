2b61baa7768843dcb87ab211f3e7c5c1046640c6
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-29 14:54:42 -0700

    read from file

--- read_from_file.py
+++ read_from_file.py
@@ -1,1 +1,13 @@
+with open("digits.txt") as file_object:
+	contents = file_object.read()
+	print(contents)
+
+	print("OR LINE BY LINE:")
+
+with open("digits.txt") as file_object:
+	for line in file_object:
+		print(line.rstrip())
+		print(len(line.rstrip()))
+
+
 
