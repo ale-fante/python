873075a3d0b7f76d01c8a06bfdc6554ad31bd5c0
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-31 13:03:31 -0700

    Car class

---
+++ car.py
@@ -0,0 +1,17 @@
+class Car():
+	""" A simple attempt to represent a car."""
+
+	def __init__(self, make, model, year):
+		"""Initialize attributes to describe a car."""
+		self.make = make
+		self.model = model
+		self.year = year
+
+	def get_descriptive_name(self):
+		"""Return a neatly formatted descriptive name."""
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+
+my_new_car = Car('honda', 'A4', 2016)
+print("YOUR CAR IS A: " + my_new_car.get_descriptive_name())
