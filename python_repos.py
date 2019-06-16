fe685edd69e2306f81b96ebc1a7b63643d00a64b
alejandra ledesma <you@example.com>
2019-06-15 21:25:36 -0700

    Requests

--- python_repos.py
+++ python_repos.py
@@ -1,9 +1,40 @@
+# Must install:
+# pip install --user requests
+
 import requests
 
 # Make an API call and store the response
 
-url = "https:// api.github.com/search/repositories?q=language:python&sort=stars"
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
 r = requests.get(url)
 
 print("Status code:" , r.status_code)
+
+# Store API response in a variable
+
+response_dict = r.json()
+
+# Process results
+print(response_dict.keys())
+
+print("Total repositories:", response_dict['total_count'])
+
+# More info about the repos
+repo_dicts = response_dict['items']
+
+print("Repositories returned", len(repo_dicts))
+
+# Examine the first repo
+
+repo_dict = repo_dicts[0]
+print("\nKeys: ", len(repo_dict))
+
+for key in sorted(repo_dict.keys()):
+	print(key)
+
+print("Selected info about the first repo:")
+
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('URL:', repo_dict['url'])
