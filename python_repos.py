4bdff4e6e1ac046f8cd4e28be3bbbca998abe849
alejandra ledesma <you@example.com>
2019-06-15 18:56:06 -0700

    Github API call

--- python_repos.py
+++ python_repos.py
@@ -1,2 +1,9 @@
 import requests
 
+# Make an API call and store the response
+
+url = "https:// api.github.com/search/repositories?q=language:python&sort=stars"
+
+r = requests.get(url)
+
+print("Status code:" , r.status_code)
